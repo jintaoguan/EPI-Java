32a45d7ad2878319732704dd057d82e08203d8bc
Jintao Guan <ilovexiao77@gmail.com>
2014-03-03 15:56:10 -0500

    add solution of problem 9.5

--- chapter09/ProblemSet_09.md
+++ chapter09/ProblemSet_09.md
@@ -5,3 +5,8 @@ Write a function that takes as input the root of a binary tree and returns true 
 
 ###Problem 9.3
 Write a function that takes as input the root of a binary tree and returns true or false depending on whether the tree is symmetric.
+
+###Problem 9.4 ------TODO-------
+
+###Problem 9.5
+Let T be the root of a binary tree in which nodes have an explicit parent field. Design an iterative algorithm that enumerate the nodes inorder and uses O(1) additional space. Your algorithm cannot modify the tree.
---
+++ chapter09/problem_09_05.java
@@ -0,0 +1,120 @@
+// Problem 9.5
+// Let T be the root of a binary tree in which nodes have an explicit
+// parent field. Design an iterative algorithm that enumerate the nodes inorder
+// and uses O(1) additional space. Your algorithm cannot modify the tree.
+
+import java.util.ArrayList;
+
+public class problem_09_05 {
+
+    // 1. recuesive solution
+    public void inorder_traversal(TreeNode root, ArrayList<Integer> ans) {
+        if( root == null ) return;
+        inorder_traversal(root.left, ans);
+        ans.add(root.data);
+        inorder_traversal(root.right, ans);
+    }
+
+    // 2. iterative solution using parent reference
+    public void inorder_traversal_parent(TreeNode root, ArrayList<Integer> ans) {
+        if( root == null ) return;
+        TreeNode prev = null;
+        TreeNode cur = root;
+        TreeNode next = null;
+
+        while( cur != null ) {
+            if( prev == null || prev.left == cur || prev.right == cur ) {
+                if( cur.left != null ) {
+                    next = cur.left;
+                } else {
+                    ans.add(cur.data);
+                    next = ( cur.right == null ? cur.parent : cur.right );
+                }
+            } else if( cur.left  == prev ) {
+                ans.add(cur.data);
+                next = ( cur.right == null ? cur.parent : cur.right );
+            } else if( cur.right == prev ) {
+                next = cur.parent;
+            }
+            prev = cur;
+            cur = next;
+        }
+    }
+
+
+    // FOLLOW-UP. 
+    // How would you perform preorder and postorder walks iteratively
+    // using O(1) additional space? Your algorithm cannot modify the tree.
+    // Nodes have an explicit parent field.
+
+
+    // recursive method
+    public void preorder_traversal(TreeNode root, ArrayList<Integer> ans) {
+        if( root == null ) return;
+        ans.add(root.data);
+        preorder_traversal(root.left, ans);
+        preorder_traversal(root.right, ans);
+    }
+
+    public void postorder_traversal(TreeNode root, ArrayList<Integer> ans) {
+        if( root == null ) return;
+        postorder_traversal(root.left, ans);
+        postorder_traversal(root.right, ans);
+        ans.add(root.data);
+    }
+
+    // iterative method
+    public void preorder_traversal_parent() {
+    }
+
+    public void postorder_traversal_parent() {
+    }
+
+
+    public static void main(String[] argv) {
+        // unit test
+        problem_09_05 solutions = new problem_09_05();
+
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t5 = new TreeNode(5);
+        TreeNode t6 = new TreeNode(6);
+        TreeNode t7 = new TreeNode(7);
+        TreeNode t8 = new TreeNode(8);
+        t1.addLeftChild(t2);
+        t1.addRightChild(t3);
+        t2.addLeftChild(t4);
+        t2.addRightChild(t5);
+        t3.addLeftChild(t6);
+        t3.addRightChild(t7);
+        t7.addLeftChild(t8);
+        // traverse using recursive method
+        ArrayList<Integer> ans1 = new ArrayList<Integer>();
+        solutions.inorder_traversal(t1, ans1);
+        System.out.println(ans1);
+        // traverse using iterative method
+        ArrayList<Integer> ans2 = new ArrayList<Integer>();
+        solutions.inorder_traversal_parent(t1, ans2);
+        System.out.println(ans2);
+
+    }
+}
+
+class TreeNode {
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode parent;
+    public int data;
+    public TreeNode(int v) { data = v; parent = null; left = null; right = null; }
+    
+    public void addLeftChild(TreeNode child) {
+        left = child;
+        child.parent = this;
+    }
+    public void addRightChild(TreeNode child) {
+        right = child;
+        child.parent = this;
+    }
+}
