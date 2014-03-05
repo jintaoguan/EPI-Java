0390879bdc5c5a3d8a8567ce5f0e96671a29dd88
Jintao Guan <ilovexiao77@gmail.com>
2014-03-04 21:02:42 -0500

    update solution of problem 9.7, add recursive method

--- chapter09/problem_09_07.java
+++ chapter09/problem_09_07.java
@@ -1,6 +1,7 @@
 
 public class problem_09_07 {
 
+    // 1. iterative method
     public TreeNode find_kth_node_binary_tree(TreeNode root, int k) {
         TreeNode cur = root;
         while( cur != null ) {
@@ -20,10 +21,52 @@ public class problem_09_07 {
         return null;
     }
 
+    // 2. recursive method
+    public TreeNode find_kth_node_binary_tree2(TreeNode root, int k) {
+        if( k > root.size )
+            return null;
+        int leftSize = 0;
+        if( root.left != null )
+            leftSize = root.left.size;
+        if( k == leftSize + 1 )
+            return root;
+        else if( k > leftSize + 1 )
+            return find_kth_node_binary_tree2(root.right, k - leftSize - 1);
+        else
+            return find_kth_node_binary_tree2(root.left, k);
+    }
+
     public static void main(String[] argv) {
         // unit test
         problem_09_07 solutions = new problem_09_07();
+        
+        TreeNode t1 = new TreeNode(1); t1.size = 7;
+        TreeNode t2 = new TreeNode(2); t2.size = 2;
+        TreeNode t3 = new TreeNode(3); t3.size = 4;
+        TreeNode t4 = new TreeNode(4); t4.size = 1;
+        TreeNode t5 = new TreeNode(5); t5.size = 2;
+        TreeNode t6 = new TreeNode(6); t6.size = 1;
+        TreeNode t7 = new TreeNode(7); t7.size = 1;
+        t1.left = t2;
+        t1.right = t3;
+        t2.right = t4;
+        t3.left = t5;
+        t3.right = t6;
+        t5.right = t7;
 
+        // iterative method
+        for( int i = 1; i <= 7; ++i ) {
+            TreeNode ans1 = solutions.find_kth_node_binary_tree(t1, i);
+            System.out.print(ans1.data + ",");
+        }
+        System.out.println();
+
+        // recursive method
+        for( int i = 1; i <= 7; ++i ) {
+            TreeNode ans2 = solutions.find_kth_node_binary_tree2(t1, i);
+            System.out.print(ans2.data + ",");
+        }
+        System.out.println();
     }
 }
 
