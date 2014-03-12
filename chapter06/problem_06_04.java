696ff077210a0b9515c0628fbd4318f2363bf223
Jintao Guan <ilovexiao77@gmail.com>
2014-03-12 16:20:04 -0400

    update solution of problem 6.4

--- chapter06/problem_06_04.java
+++ chapter06/problem_06_04.java
@@ -9,7 +9,7 @@
 public class problem_06_04 {
 
     // solution for problem (1)
-    public int find_sum_two_pairs( int[] A ) {
+    public int find_max_two_pairs( int[] A ) {
         if( A.length <= 1 ) 
             return 0;
         int[] left = new int[A.length];
@@ -38,7 +38,7 @@ public class problem_06_04 {
     }
 
     // solution for problem (2)
-    public int find_sum_k_pairs( int[] A, int k ) {
+    public int find_max_k_pairs( int[] A, int k ) {
         int[][] dp = new int[k+1][A.length+1];
 
         int[] arr = new int[A.length+1];
@@ -66,12 +66,53 @@ public class problem_06_04 {
         return dp[k][A.length];
     }
 
+    // another format of solution for problem (2)
+    public int find_max_k_pairs2( int[] A, int k ) {
+
+        int[][] dp = new int[A.length+1][k+1];
+        int[] arr = new int[A.length+1];
+        arr[0] = 0;
+
+        for( int i = 1; i <= A.length; ++i )
+            arr[i] = A[i-1];
+
+        for( int i = 0; i <= k; ++i ) {
+            dp[0][i] = 0;
+            dp[1][i] = 0;
+        }
+        for( int i = 0; i <= A.length; ++i ) {
+            dp[i][0] = 0;
+        }
+
+        for( int i = 1; i <= k; ++i ) {
+            int max = dp[1][i-1] - arr[1];
+            for( int j = 2; j <= A.length; ++j ) {
+                dp[i][j] = Math.max( dp[j-1][i], max + arr[j] );
+                max = Math.max( max, dp[j][i-1] - arr[j] );
+            }
+        }
+        return dp[A.length][k];
+    }
+
+    // solution for problem (3)
+    public int find_max_any_pairs( int[] prices ) {
+        if( prices.length == 0 ) return 0;
+        int profit = 0;
+        for( int i = 0; i < prices.length - 1; ++i )
+        {
+            if( prices[i] < prices[i+1] )
+                profit += prices[i+1] - prices[i];
+        }
+        return profit;
+    }
 
     public static void main(String[] argv) {
         problem_06_04 solutions = new problem_06_04();
         int[] A = new int[]{ 1, -2, 2, 0, 3, 1, 2, 3 };
-        System.out.println( solutions.find_sum_two_pairs(A) );
-        System.out.println( solutions.find_sum_k_pairs(A,3) );
+        System.out.println( solutions.find_max_two_pairs(A) );
+        System.out.println( solutions.find_max_k_pairs(A,3) );
+        System.out.println( solutions.find_max_any_pairs(A) );
+        //System.out.println( solutions.find_sum_k_pairs2(A,3) );
     }
 
 }
